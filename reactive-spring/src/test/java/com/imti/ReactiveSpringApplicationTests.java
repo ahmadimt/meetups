21e644bf3304c572c6c9ba5e0bd58592646522b0
Imteyaz Ahmad <ahmad.imt07@gmail.com>
2018-08-22 11:37:56 +0530

    project for reactive spring meetup

---
+++ reactive-spring/src/test/java/com/imti/ReactiveSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
